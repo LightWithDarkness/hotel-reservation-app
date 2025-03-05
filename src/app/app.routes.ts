3ba885249a4762ed6b60810752697175c37e55c1
Mannu Kumar Verma <lightwithdarkness@Mannus-MacBook-Pro.local>
2025-03-05 19:07:40 +0530

    created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "new", component:ReservationFormComponent }, 
+    // {path:"all-reservations",loadChildren:()=>import('./reservation/reservation.module').then(m=>m.ReservationModule)}, //lazy loading
+    {path:"all",component:ReservationListComponent}
+];
