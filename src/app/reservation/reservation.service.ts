97e27b70edfb2caa93a30b659fb166d434688b9b
Mannu Kumar Verma <lightwithdarkness@Mannus-MacBook-Pro.local>
2025-03-06 12:54:23 +0530

    Created reservation services

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,24 @@ import { Injectable } from '@angular/core';
 export class ReservationService {
 
   constructor() { }
+
+  private reservations: Reservation[] = [];
+
+  //CRUD
+  getAllReservations(): Reservation[] {
+    return this.reservations;
+  }
+  getReservationById(id: string) {
+    return this.reservations.find(rsv => rsv.id === id)
+  }
+  addReservation(reservation: Reservation) {
+    this.reservations.push(reservation);
+  }
+  deleteReservation(id: string) {
+    this.reservations = this.reservations.filter(rsv => rsv.id !== id);
+  }
+  updateReservation(reservation: Reservation) {
+    this.reservations = this.reservations.map(rsv => rsv.id === reservation.id ? reservation : rsv)
+  }
+
 }
